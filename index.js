c1dd722d5dc84fc1353baf05ce7a088ee98dbc73
lycho33 <cho.lyds@gmail.com>
2021-08-24 21:52:16 -0400

    commit tree 1467d165253aaa164ac2287fff604adfb599c0d7

--- index.js
+++ index.js
@@ -10,4 +10,39 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+// function greet(string) {
+//   let intString = parseInt(string.split(':'))
+//   if (intString < 12) {
+//     return 'Good Morning'
+//   } else if (intString > 12 && intString < 17) {
+//       return 'Good Afternoon'
+//   } else {
+//       return 'Good Evening'
+//   }
+// }
+
+// function displayMessage(string) {
+//   let element = document.getElementById('greeting')
+//   element.innerText = string
+// }
 /* Write your implementation of displayMessage() */
+
+
+
+
+function displayMessage(greet){
+  let greeting = document.querySelector('#greeting')
+  greeting.innerText = greet
+}
+
+function greet(timeString){
+  let parsedTime = parseInt(timeString.split(':'))
+  console.log(parsedTime)
+  if (parsedTime < 12){
+    return 'Good Morning'
+  } else if (parsedTime == 12 || parsedTime < 18){
+    return 'Good Afternoon'
+  } else{
+    return 'Good Evening'
+  }
+}
